72407f1b55d9d0f27265b2c59d5fdc52f7a88111
Adam Hassall <ajhassall2000@gmail.com>
2025-03-21 21:30:55 +0000

    InitialCommit: basic project outline

---
+++ ContentManagementApiTests/UnitTest1.cs
@@ -0,0 +1,15 @@
+namespace ContentManagementApiTests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}

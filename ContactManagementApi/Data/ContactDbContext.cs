03c7d6a55e7c40bead928c9ef329de4fa7058237
Adam Hassall <ajhassall2000@gmail.com>
2025-03-27 22:13:02 +0000

    fixed error

--- ContactManagementApi/Data/ContactDbContext.cs
+++ ContactManagementApi/Data/ContactDbContext.cs
@@ -8,7 +8,7 @@ namespace ContactManagementApi.Data
 
         public DbSet<Contact> Contacts { get; set; }
         public DbSet<Fund> Funds { get; set; }
-        public DbSet<ContactFundAssignment> ContactFundAssignments {get; set;}
+        public DbSet<FundRelationship> ContactFundAssignments {get; set;}
         public DbSet<Trade> Trades {get; set;}
     }
 }
